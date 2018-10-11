de0f417362d2e060bb78cda41ede30b4db227d08
unknown <bergros.gigja@gmail.com>
2018-10-11 17:05:12 +0000

    Fix conflicts and update index.js routes and body-parser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 const app = express();
 const port = 3000;
 const ArtistService = require('./services/artistService');
@@ -29,8 +31,15 @@ app.get('/api/artists/:id', (req, res) => {
     //TODO: implement get artist by id
 });
 
-app.post('/api/artists', (req, res) => {
-    //TODO: implement create new artist
+router.post('/artists', (req, res) => {
+    const {body} = req;
+    const artistService = new ArtistService();
+
+    artistService.on(artistService.events.CREATE_ARTIST, data => {
+        res.json(data);
+    });
+
+    artistService.createArtist(body);
 });
 
 app.get('/api/customers', (req, res) => {
@@ -73,6 +82,8 @@ app.post('/api/auctions/:id/bids', (req, res) => {
     //TODO: implement create new auction bid
 });
 
+app.use(bodyParser.json());
+app.use('/api', router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
