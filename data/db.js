0305267f499bc53d7b2b02505a50002ab2bf681f
unknown <bergros.gigja@gmail.com>
2018-10-11 15:30:12 +0000

    Fixing merge

--- data/db.js
+++ data/db.js
@@ -14,7 +14,6 @@ module.exports = {
     Auction: connection.model('Auction', auctionSchema),
     AuctionBid: connection.model('AuctionBid', auctionBidSchema),
     Customer: connection.model('Customer', customerSchema),
-<<<<<<< HEAD
     connection
     //Order: connection.model('Order', orderSchema)
 };
