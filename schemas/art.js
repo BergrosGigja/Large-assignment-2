80e7a180aaa2fafc7816c5ac0a443a6a03f23bf8
unknown <bergros.gigja@gmail.com>
2018-10-14 00:53:18 +0000

    Minor fixes in art and artist schemas

--- schemas/art.js
+++ schemas/art.js
@@ -1,10 +1,9 @@
 const Schema = require('mongoose').Schema;
-var ObjectId = Schema.Types.ObjectId;
 
 module.exports = new Schema({
     title: { type: String, required: true },
-    artistId: { type: ObjectId, required: true },
-    date: { type: Date, required: true, default: Date.now },
+    artistId: { type: Schema.Types.ObjectId, required: true, ref: 'Artist' },
+    date: { type: Date, required: true, default: Date.now() },
     images: { type: [String], required: false},
     description: { type: String, required: false },
     isAuctionItem: { type: Boolean, required: false, default: false }
